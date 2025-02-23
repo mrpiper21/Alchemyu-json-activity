ce68cc0da657e1dca6a141ad392a1450f3d0bec7
Bernard <benbaah104@gmail.com>
2025-02-23 18:41:33 +0000

    update

--- method/getTotalTransaction.js
+++ method/getTotalTransaction.js
@@ -3,19 +3,19 @@ const { API_KEY } = process.env;
 const axios = require('axios');
 const url = `https://eth-mainnet.g.alchemy.com/v2/${API_KEY}`;
 
-async function getNonce(address) {
+async function getTotalTransactions(blockNumber) {
     const response = await axios.post(url, {
-			jsonrpc: "2.0",
-			id: 1,
-			method: "eth_getTransactionCount", // <-- fill in the method
-			params: [address, "latest"], // <-- fill in the params
-		});
+        jsonrpc: "2.0",
+        id: 1,
+        method: "eth_getBlockTransactionCountByNumber",
+        params: [blockNumber, "safe"], 
+    });
     
     // use this if you want to inspect the response data!
     // console.log(response.data);
 
-    // return the nonce for the address
+    // return the total number of transactions in the block
     return response.data.result;
 }
 
-module.exports = getNonce;
+module.exports = getTotalTransactions;
